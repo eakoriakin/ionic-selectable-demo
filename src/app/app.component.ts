3f77c558c0c8b2b949ece8d8b19ff92f480d970f
Evgenii Koriakin <eakoriakin@yandex.ru>
2018-02-11 10:44:09 +0400

    Remove tabs layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { TabsPage } from '../pages/tabs/tabs';
+import { HomePage } from '../pages/home/home';
 
 @Component({
     templateUrl: 'app.html'
 })
 export class MyApp {
-    rootPage: any = TabsPage;
+    rootPage: any = HomePage;
 
     constructor(private platform: Platform, private statusBar: StatusBar, private splashScreen: SplashScreen) {
         this.platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { SelectSearchableModule } from 'ionic-select-searchable';
@@ -13,10 +10,7 @@ import { SelectSearchableModule } from 'ionic-select-searchable';
 @NgModule({
     declarations: [
         MyApp,
-        AboutPage,
-        ContactPage,
-        HomePage,
-        TabsPage
+        HomePage
     ],
     imports: [
         BrowserModule,
@@ -26,10 +20,7 @@ import { SelectSearchableModule } from 'ionic-select-searchable';
     bootstrap: [IonicApp],
     entryComponents: [
         MyApp,
-        AboutPage,
-        ContactPage,
-        HomePage,
-        TabsPage
+        HomePage
     ],
     providers: [
         StatusBar,
